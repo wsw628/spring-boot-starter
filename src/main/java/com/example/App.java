ec5295f845367caad5ff5eca8f80f4c3374ad10c
wsw628 <wsw628@naver.com>
2018-11-26 00:06:33 +0900

    implements CommandLineRunner

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,47 +3,31 @@ package com.example;
 import com.example.app.Argument;
 import com.example.app.ArgumentResolver;
 import com.example.app.Calculator;
-import com.example.app.Frontend;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-
-import java.util.Scanner;
 
 @EnableAutoConfiguration
-//@Import(AppConfig.class)
 @ComponentScan
-public class App {
+public class App implements CommandLineRunner {
+
+    @Autowired
+    ArgumentResolver argumentResolver;
+
+    @Autowired
+    Calculator calculator;
+
+    @Override
+    public void run(String... strings) {
+        System.out.println("Enter 2 numbers like 'a b' : ");
+        Argument argument = argumentResolver.resolve(System.in);
+        int result = calculator.calc(argument.getA(), argument.getB());
+        System.out.println("result = " + result);
+    }
+
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-
-        try (ConfigurableApplicationContext context =
-                     SpringApplication.run(App.class, args)) {
-
-            /**
-             * ArgumentResolver 를 통해 아래 부분도 추상화가 되었다
-             *
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter 2 numbers like 'a b' : ");
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            */
-
-            /**
-             * Frontend 를 통해 다시 한번 추상화 된다
-             *
-            System.out.print("Enter 2 numbers like 'a b' : ");
-            ArgumentResolver argumentResolver = context.getBean(ArgumentResolver.class);
-            Argument argument = argumentResolver.resolve(System.in);
-            Calculator calculator = context.getBean(Calculator.class);
-            int result = calculator.calc(argument.getA(), argument.getB());
-            System.out.println("result : " + result);
-            */
-
-            Frontend frontend = context.getBean(Frontend.class);
-            frontend.run();
-        }
+        SpringApplication.run(App.class);
     }
 }
