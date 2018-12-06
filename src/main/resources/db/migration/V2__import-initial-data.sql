134fd0bbef2445e80cbc6ceb1d53f03363bd9f7a
wsw628 <wsw628@naver.com>
2018-12-06 23:37:54 +0900

    add migration script

---
+++ src/main/resources/db/migration/V2__import-initial-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers (first_name, last_name) VALUES ('Nobita', 'Nobi');
+INSERT INTO customers (first_name, last_name) VALUES ('Takesi', 'Goda');
+INSERT INTO customers (first_name, last_name) VALUES ('Suneo', 'Honekawa');
+INSERT INTO customers (first_name, last_name) VALUES ('Shizuka', 'Minamoto');
